560567dc6d2c6e6c66121a228973b9354ab9c06c
rushabhmahant <rushabhmahant555@gmail.com>
2023-01-11 22:37:31 +0530

    [#007] SpringBoot with  Microservices dependencies

---
+++ DemoMicroservice/src/test/java/com/cts/demomicroservice/DemoMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.demomicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
